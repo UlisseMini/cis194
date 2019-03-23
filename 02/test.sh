75de51b5c63b933c7c609c3d8475e60674781315
Ulisse Mini <ulisse.mini@gmail.com>
2019-03-23 12:31:30 -0400

    yeah i should really read the docs

--- 02/test.sh
+++ 02/test.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # tfw you dont want to use a stack project
 
-file="Test.hs"
-cmd=$(cat <<-EOF
-    echo ":!clear
-          main
-         " | ghci $file -package QuickCheck -package hspec
-EOF)
-
-echo "./LogAnalysis.hs" | entr -c bash -c "$cmd"
+echo "./LogAnalysis.hs" | entr -c ghci "./Test.hs" -e "main" -package QuickCheck -package hspec
