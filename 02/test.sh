10cf135209981da3c9638bd1006e0103dd0ee9c0
Ulisse Mini <ulisse.mini@gmail.com>
2019-03-26 19:04:45 -0400

    Retest on any .hs files

--- 02/test.sh
+++ 02/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # tfw you dont want to use a stack project
 
-echo "./LogAnalysis.hs" | entr -c ghci "./Test.hs" -e "main" -package QuickCheck -package hspec
+#echo "./LogAnalysis.hs"
+ls *.hs | entr -c ghci "./Test.hs" -e "main" -package QuickCheck -package hspec
